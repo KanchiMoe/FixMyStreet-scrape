00261b296cfaa955d848a8458ab851adfed6f861
KanchiMoe <github@chiyo-chan.me>
2025-06-02 17:12:27 +0100

    add new sql functions to __init__

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 from .check_number_in_db import is_number_in_db
 from .get_fms_report_page import get_report_page
-from .sql_db_actions import SQL_insert_into_db, SQL_count_number_of_rows, truncate, SQL_get_UPPER_NUMBER
+from .sql_db_actions import SQL_insert_into_db, SQL_count_number_of_rows, truncate, SQL_get_UPPER_NUMBER, SQL_update_upper_number, SQL_check_autofind_should_run
 from .get_report_contents import process_report_content
 from .get_randomnumber import get_random_number
 from .strategies import sequential_strategy, single_strategy, random_strategy
